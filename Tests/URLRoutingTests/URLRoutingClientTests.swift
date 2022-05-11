660e25dd54d99cc3a87d54926f462de0803d3df8
stephencelis <stephencelis@users.noreply.github.com>
2022-05-11 15:31:21 +0000

    Run swift-format

--- Tests/URLRoutingTests/URLRoutingClientTests.swift
+++ Tests/URLRoutingTests/URLRoutingClientTests.swift
@@ -8,51 +8,53 @@ import XCTest
 
 class URLRoutingClientTests: XCTestCase {
   #if os(macOS) || os(iOS) || os(tvOS) || os(watchOS)
-  @available(iOS 13, macOS 10.15, tvOS 13, watchOS 6, *)
-  func testJSONDecoder_noDecoder() async throws {
-    struct Response: Equatable, Decodable {
-      let decodableValue: String
+    @available(iOS 13, macOS 10.15, tvOS 13, watchOS 6, *)
+    func testJSONDecoder_noDecoder() async throws {
+      struct Response: Equatable, Decodable {
+        let decodableValue: String
+      }
+      enum AppRoute {
+        case test
+      }
+      let sut = URLRoutingClient<AppRoute>(request: { _ in
+        ("{\"decodableValue\":\"result\"}".data(using: .utf8)!, URLResponse())
+      })
+      let response = try await sut.request(.test, as: Response.self)
+      XCTAssertEqual(response.value, .init(decodableValue: "result"))
     }
-    enum AppRoute {
-      case test
+    @available(iOS 13, macOS 10.15, tvOS 13, watchOS 6, *)
+    func testJSONDecoder_customDecoder() async throws {
+      struct Response: Equatable, Decodable {
+        let decodableValue: String
+      }
+      enum AppRoute {
+        case test
+      }
+      let customDecoder = JSONDecoder()
+      customDecoder.keyDecodingStrategy = .convertFromSnakeCase
+      let sut = URLRoutingClient<AppRoute>(
+        request: { _ in
+          ("{\"decodable_value\":\"result\"}".data(using: .utf8)!, URLResponse())
+        }, decoder: customDecoder)
+      let response = try await sut.request(.test, as: Response.self)
+      XCTAssertEqual(response.value, .init(decodableValue: "result"))
     }
-    let sut = URLRoutingClient<AppRoute>(request: { _ in
-      ("{\"decodableValue\":\"result\"}".data(using: .utf8)!, URLResponse())
-    })
-    let response = try await sut.request(.test, as: Response.self)
-    XCTAssertEqual(response.value, .init(decodableValue: "result"))
-  }
-  @available(iOS 13, macOS 10.15, tvOS 13, watchOS 6, *)
-  func testJSONDecoder_customDecoder() async throws {
-    struct Response: Equatable, Decodable {
-      let decodableValue: String
+    @available(iOS 13, macOS 10.15, tvOS 13, watchOS 6, *)
+    func testJSONDecoder_customDecoderForRequest() async throws {
+      struct Response: Equatable, Decodable {
+        let decodableValue: String
+      }
+      enum AppRoute {
+        case test
+      }
+      let customDecoder = JSONDecoder()
+      customDecoder.keyDecodingStrategy = .convertFromSnakeCase
+      let sut = URLRoutingClient<AppRoute>(
+        request: { _ in
+          ("{\"decodableValue\":\"result\"}".data(using: .utf8)!, URLResponse())
+        }, decoder: customDecoder)
+      let response = try await sut.request(.test, as: Response.self, decoder: .init())
+      XCTAssertEqual(response.value, .init(decodableValue: "result"))
     }
-    enum AppRoute {
-      case test
-    }
-    let customDecoder = JSONDecoder()
-    customDecoder.keyDecodingStrategy = .convertFromSnakeCase
-    let sut = URLRoutingClient<AppRoute>(request: { _ in
-      ("{\"decodable_value\":\"result\"}".data(using: .utf8)!, URLResponse())
-    }, decoder: customDecoder)
-    let response = try await sut.request(.test, as: Response.self)
-    XCTAssertEqual(response.value, .init(decodableValue: "result"))
-  }
-  @available(iOS 13, macOS 10.15, tvOS 13, watchOS 6, *)
-  func testJSONDecoder_customDecoderForRequest() async throws {
-    struct Response: Equatable, Decodable {
-      let decodableValue: String
-    }
-    enum AppRoute {
-      case test
-    }
-    let customDecoder = JSONDecoder()
-    customDecoder.keyDecodingStrategy = .convertFromSnakeCase
-    let sut = URLRoutingClient<AppRoute>(request: { _ in
-      ("{\"decodableValue\":\"result\"}".data(using: .utf8)!, URLResponse())
-    }, decoder: customDecoder)
-    let response = try await sut.request(.test, as: Response.self, decoder: .init())
-    XCTAssertEqual(response.value, .init(decodableValue: "result"))
-  }
   #endif
 }
