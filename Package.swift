ebf135360ca64b56f31e34d397324f1263248cb3
Stephen Celis <stephen@stephencelis.com>
2023-07-30 15:36:30 -0700

    Bump dependencies

--- Package.swift
+++ Package.swift
@@ -17,8 +17,8 @@ let package = Package(
     .package(url: "https://github.com/apple/swift-argument-parser", from: "0.5.0"),
     .package(url: "https://github.com/apple/swift-collections", from: "1.0.3"),
     .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.0.0"),
-    .package(url: "https://github.com/pointfreeco/swift-parsing", from: "0.12.0"),
-    .package(url: "https://github.com/pointfreeco/xctest-dynamic-overlay", from: "0.3.0"),
+    .package(url: "https://github.com/pointfreeco/swift-parsing", from: "0.13.0"),
+    .package(url: "https://github.com/pointfreeco/xctest-dynamic-overlay", from: "1.0.0"),
     .package(url: "https://github.com/google/swift-benchmark", from: "0.1.1"),
   ],
   targets: [
