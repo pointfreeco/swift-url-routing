1cfd564259ecb1d324bb718a8f03e513dab738d2
Stephen Celis <stephen@stephencelis.com>
2024-07-24 13:53:23 -0700

    Point Issue Reporting to xctest-dynamic-overlay URL (#105)

--- Package.swift
+++ Package.swift
@@ -17,15 +17,15 @@ let package = Package(
     .package(url: "https://github.com/apple/swift-argument-parser", from: "0.5.0"),
     .package(url: "https://github.com/apple/swift-collections", from: "1.0.3"),
     .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.0.0"),
-    .package(url: "https://github.com/pointfreeco/swift-issue-reporting", from: "1.2.0"),
     .package(url: "https://github.com/pointfreeco/swift-parsing", from: "0.13.0"),
+    .package(url: "https://github.com/pointfreeco/xctest-dynamic-overlay", from: "1.2.2"),
     .package(url: "https://github.com/google/swift-benchmark", from: "0.1.1"),
   ],
   targets: [
     .target(
       name: "URLRouting",
       dependencies: [
-        .product(name: "IssueReporting", package: "swift-issue-reporting"),
+        .product(name: "IssueReporting", package: "xctest-dynamic-overlay"),
         .product(name: "OrderedCollections", package: "swift-collections"),
         .product(name: "Parsing", package: "swift-parsing"),
       ]
